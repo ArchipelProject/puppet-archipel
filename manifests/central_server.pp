18187714a0b9ac398cb8341febb0df61b6943782
Nicolas Ochem <nicolas.ochem@gmail.com>
2014-04-14 22:38:30 -0700

    add admin

--- manifests/central_server.pp
+++ manifests/central_server.pp
@@ -1,4 +1,9 @@
 class archipel::central_server{
   include ejabberd
   ejabberd::contrib::module{ 'mod_xmlrpc': }
+
+  ejabberd_user { 'admin':
+    host        => 'archipel_central_server.archipel.priv',
+    password    => 'admin'
+  }
 }
