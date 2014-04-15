90825315d2b85403b8df896a68f4fb8bf0f2e390
Nicolas Ochem <nicolas.ochem@gmail.com>
2014-04-15 00:09:43 -0700

    fix location of ejabberd.cfg

--- manifests/central_server.pp
+++ manifests/central_server.pp
@@ -1,6 +1,6 @@
 class archipel::central_server{
   class { 'ejabberd':
-    config_source   => 'puppet:///files/ejabberd.cfg',
+    config_source   => 'puppet:///modules/archipel/ejabberd.cfg',
     package_ensure  => 'installed',
     package_name    => 'ejabberd',
     service_reload  => true,
